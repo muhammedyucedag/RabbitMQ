d0732377f4b276f44190ab19b7b8bfb1d23a7919
Muhammed Yücedağ <muhammedyucedagg@gmail.com>
2024-02-15 22:26:45 +0300

    Son güncelleme clearing

--- RabbitMQ.subcriber/Program.cs
+++ RabbitMQ.subcriber/Program.cs
@@ -20,9 +20,6 @@ class Program
             Uri = !string.IsNullOrEmpty(amqpUrl) ? new Uri(amqpUrl) : null
         };
 
-        //var factory = new ConnectionFactory();
-        //factory.Uri = new Uri("amqps://irldprra:pvfLdiVq_X1fS2uKIaz9NPHHRG5PENIo@gull.rmq.cloudamqp.com/irldprra");
-
         // RabbitMQ serverına bağlanma
         using var connection = factory.CreateConnection();
         Console.WriteLine("RabbitMQ serverına bağlanıldı.");
@@ -51,81 +48,4 @@ class Program
         Console.ReadLine();
     }
 
-
 }
-
-//public class Program
-//{
-//    public static void Main(string[] args)
-//    {
-//        var builder = new ConfigurationBuilder();
-//        builder.SetBasePath(Directory.GetCurrentDirectory())
-//           .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-
-//        IConfiguration config = builder.Build();
-
-//        var amqpUrl = config["AMQP-URL"];
-
-//        var factory = new ConnectionFactory()
-//        {
-//            Uri = !string.IsNullOrEmpty(amqpUrl) ? new Uri(amqpUrl) : null
-//        };
-
-//        // RabbitMQ serverına bağlanma
-//        using (var connection = factory.CreateConnection())
-//        {
-//            // Bağlantı kurulduğunda yapılabilecek işlemler burada gerçekleştirilir
-//            Console.WriteLine("RabbitMQ serverına bağlanıldı.");
-//            Console.WriteLine("Bağlantı URL'si: " + amqpUrl);
-
-//            // RabbitMQ kanal üzerinden bağlanma
-//            using (var channel = connection.CreateModel())
-//            {
-//                // Kuyruk Oluşturma
-//                //channel.QueueDeclare("hello-queue", true, false, true);
-
-//                //True dersek her subscriber'e bölüştürür mesajları.
-//                //False dersek her subscriber'e 1 1 1 dağıtacak.
-
-//                //Random kuyruk oluşturma
-
-//                var randomQueueName = "log-database-save-queue"; //channel.QueueDeclare().QueueName;
-
-//                // Kuyruğu oluştur
-//                channel.QueueDeclare(randomQueueName, true, false, false);
-
-//                // Fanout exchange ile kuyruğu bağla
-//                channel.QueueBind(randomQueueName, "logs-fanout", "", null);
-
-//                // Ön yükleme ayarını yap
-//                channel.BasicQos(0, 10, false);
-
-//                Console.WriteLine("Waiting for messages");
-
-//                // Tüketici oluştur
-//                var consumer = new EventingBasicConsumer(channel);
-
-//                // False ile silme işlemini hemen yapmıyoruz. BasicAck() ile ulaşan mesajı sileceğiz
-//                // Kuyruktan mesajları al
-//                channel.BasicConsume(randomQueueName, false, consumer);
-
-//                Console.WriteLine("Log dinleniyor");
-
-//                consumer.Received += (model, e) =>
-//                {
-//                    var message = Encoding.UTF8.GetString(e.Body.ToArray());
-
-//                    Thread.Sleep(1000);
-
-//                    Console.WriteLine($"Received: {message}");
-
-//                    channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: false);
-//                };
-//            }
-
-//        }
-
-//        Console.ReadLine();
-//    }
-
-//}
