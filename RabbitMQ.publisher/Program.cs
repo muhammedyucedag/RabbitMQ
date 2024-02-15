e28630c74f523cc5143f778a63b217c3e347fc60
Muhammed Yücedağ <muhammedyucedagg@gmail.com>
2024-02-15 15:02:13 +0300

    Subcriber ve Publisher sistemi

--- RabbitMQ.publisher/Program.cs
+++ RabbitMQ.publisher/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using RabbitMQ.Client;
+using System.Text;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = new ConfigurationBuilder();
+        builder.SetBasePath(Directory.GetCurrentDirectory())
+           .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+        IConfiguration config = builder.Build();
+
+        var amqpUrl = config["AMQP-URL"];
+
+        var factory = new ConnectionFactory()
+        {
+            Uri = new Uri(amqpUrl)
+        };
+
+        // RabbitMQ serverına bağlanma
+        using (var connection = factory.CreateConnection())
+        {
+            // Bağlantı kurulduğunda yapılabilecek işlemler burada gerçekleştirilir
+            Console.WriteLine("RabbitMQ serverına bağlanıldı.");
+            Console.WriteLine("Bağlantı URL'si: " + amqpUrl);
+
+            // RabbitMQ kanal üzerinden bağlanma
+            using (var channel = connection.CreateModel())
+            {
+                // Kuyruk Oluşturma
+                channel.QueueDeclare("hello-queue", true, false, true);
+
+                string message = "Hello World RabbitMQ";
+
+                // Mesajı Byte halinde alıyoruz
+                var messageBody = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+
+                Console.WriteLine("Mesaj Gönderilmiştir.");
+            }
+        }
+
+        Console.ReadLine();
+    }
+}
